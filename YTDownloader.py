c01af5fb103b5b5af4f885cac961a89d99070bab
Luis Pöller <luis.poeller@gmx.de>
2020-10-20 18:58:13 +0200

    First draft

--- YTDownloader.py
+++ YTDownloader.py
@@ -6,3 +6,12 @@ Created on Tue Oct 20 18:45:53 2020
 @author: delta-coder
 """
 
+from pytube import YouTube
+
+def Downloader(link):
+    url = YouTube(link)
+    video = url.streams.first()
+    video.download()
+
+if __name__ == "__main__":
+    Downloader("https://www.youtube.com/watch?v=tD6Zm_1ulYU")
